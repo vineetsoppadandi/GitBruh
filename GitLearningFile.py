362e37198981e51d1dfea9c41223d724fdbba362
Vineet Soppadandi <52617740+vineetsoppadandi@users.noreply.github.com>
2022-07-30 22:43:52 +1000

    Update GitLearningFile.py

--- GitLearningFile.py
+++ GitLearningFile.py
@@ -1,4 +1,6 @@
 print('Branch:', 1)
 print("Post Master commit - 2nd change to test the change feature")
 print("Extra edit in the main branch")
-print("R1-Edit Main")
+print("W1-Edit Main")
+print("W2-Edit Main")
+
