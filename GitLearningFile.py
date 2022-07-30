809de3ea79eb2045f39316c3f8c730429cfce945
Vineet Soppadandi <52617740+vineetsoppadandi@users.noreply.github.com>
2022-07-30 23:20:36 +1000

    Commit - LocalBR1-W1 Change

--- GitLearningFile.py
+++ GitLearningFile.py
@@ -1,6 +1,8 @@
 print('Branch:', 1)
 print("Post Master commit - 2nd change to test the change feature")
 print("Extra edit in the main branch")
-print("W1-Edit Main")
-print("W2-Edit Main")
+print("R1-Edit1 Main")
+print("R1-Edit2 Main")
+print("LocalBranch1 - w1")
+
 
